c938ba3c623991e588b254c7a381d331813525cc
Jeremy Josol <jeremyjosol@gmail.com>
2023-08-11 13:45:20 -0700

    Update README.md content

--- js/scripts.js
+++ js/scripts.js
@@ -12,25 +12,25 @@ function userInput(event){
   } else if (questionJava && questionJava2 === "No" && questionCSharp === "Yes" && questionPython === "Indentation" && questionRuby === "Kyogre") {
     result = "C#! Python could have potential for you in the future. Pokemon Sapphire is fun!"  
   } else if (questionJava === "No" && questionJava2 === "No" &&questionCSharp === "No" && questionPython === "Curly braces" && questionRuby === "Groudon") {
-    result = "So, you want to learn Ruby... I didn't think you would make it this far..."
+    result = "A rare result, Pokemon Ruby."
   } else if (questionJava === "Yes" && questionJava2, questionCSharp === "No" && questionPython === "Curly braces" && questionRuby === "Groudon") {
     result = "Javascript! Team Groudon!"
   } else if (questionJava === "Yes" && questionJava2 === "No" &&questionCSharp === "No" && questionPython === "Curly braces" && questionRuby === "Kyogre") {
     result = "Javascript! Team Kyogre!"
   } else if (questionJava === "No" && questionJava2 === "No" && questionCSharp === "No" && questionPython === "Curly braces" && questionRuby === "Kyogre") { 
-    result = "There's many different languages you can learn with curly braces. Kyogre!!!"
+    result = "There's many different languages you can learn with curly braces. Team Kyogre!"
   } else if (questionJava && questionJava2 === "Yes" && questionCSharp === "No" && questionPython === "Indentation" && questionRuby === "Kyogre") {
     result = "Javascript! Python could have some potential in the future too. Kyogre is awesome. :)"
   } else if (questionJava === "Yes" && questionJava2 && questionCSharp === "No" && questionPython ==="Indentation" && questionRuby === "Kyogre") {
     result = "Javascript! Think about Python in the future too. Team Kyogre!"
   } else if (questionJava && questionJava2 && questionCSharp === "No" && questionPython === "Indentation" && questionRuby === "Kyogre") {
-    result = "Python. Kyogre!!!"
+    result = "Python. Team Kyogre!"
   } else if (questionJava && questionJava2 && questionCSharp === "Yes" && questionPython === "Curly braces" && questionRuby === "Kyogre") {
-    result = "You should try Javascript AND C#. Check out Epicodus and Pokemon Sapphire!"
+    result = "You should try Javascript AND C#. Check out Epicodus and Pokemon Sapphire while you learn!"
   } else if (questionJava && questionJava2 && questionCSharp === "Yes" && questionPython === "Curly braces" && questionRuby === "Groudon") {
     result = "You should try Javascript AND C#. Check out Epicodus! Pokemon Ruby is fun too."
-  } else if (questionJava && questionCSharp === "Yes" && questionJava2 === "No" && questionPython === "Curly braces") {
-    result = "You should try Javascript AND C#. Check out Epicodus!"
+  } else if (questionJava && questionCSharp === "Yes" && questionJava2 === "No" && questionPython === "Curly braces" &&questionRuby === "Kyogre") {
+    result = "You should try Javascript AND C#. Check out Epicodus and Pokemon Sapphire while you learn!"
   } else if (questionJava && questionCSharp === "Yes" && questionJava2 === "No" && questionPython === "Indentation" && questionRuby === "Groudon") {
     result = "You should try Javascript AND C#. Depending on what you master, it could be worth it to check out Python in the future."
   } else if (questionJava && questionJava2 === "Yes" && questionCSharp === "No" && questionPython === "Indentation" && questionRuby === "Groudon") {
